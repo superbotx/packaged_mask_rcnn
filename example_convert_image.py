a5cc06f7446fdd955c7a8b9d06565858c943f233
tianhaoz95 <tianhaoz@umich.edu>
2018-04-01 18:00:58 -0700

    update

--- example_convert_image.py
+++ example_convert_image.py
@@ -4,7 +4,7 @@ from mask_rcnn.ez import EZ
 
 ez = EZ()
 
-image_filename = 'sample_images/sample2.jpg'
+image_filename = 'sample_images/sample4.jpg'
 sample_image_input = skimage.io.imread(image_filename)
 sample1_result, info = ez.detect(sample_image_input)
 skimage.io.imsave('output_image.jpg', sample1_result)
